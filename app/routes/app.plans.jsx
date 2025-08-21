7993fcff76b453ac81acbb08ee258f2c7c4ac3e3
Safwan Adnan <safwanadnan19@gmail.com>
2025-08-21 14:31:12 +0500

    refactor: simplify PlansPage component to create a hosted session on mount

--- app/routes/app.plans.jsx
+++ app/routes/app.plans.jsx
@@ -1,34 +1,13 @@
-import { Layout, Page } from "@shopify/polaris";
-import { TitleBar } from "@shopify/app-bridge-react";
-import { PlanCardStack, PlanCardType } from "@heymantle/polaris";
 import { useMantle } from "@heymantle/react";
+import { useEffect } from 'react';
 
-export default function PlansPage() {
-  const { customer, plans, client } = useMantle();
-
-  const handleSelectPlan = async ({ plan, discount }) => {
-    const subscription = await client.subscribe({
-      planId: plan.id,
-      discountId: discount?.id,
-      returnUrl: "/app/plans",
-    });
-    const url = subscription.confirmationUrl;
-    open(url, "_top");
-  };
-
-  return (
-    <Page>
-      <TitleBar title="Select a plan" />
-      <Layout>
-        <Layout.Section>
-          <PlanCardStack
-            cardType={PlanCardType.Highlighted}
-            customer={customer}
-            plans={plans}
-            onSelectPlan={handleSelectPlan}
-          />
-        </Layout.Section>
-      </Layout>
-    </Page>
-  );
-} 
+export default function () {
+  const { createHostedSession } = useMantle();
+  useEffect(() => {
+    (async () => {
+      const session = await createHostedSession({ type: 'account' });
+      open(session.url, "_self");
+    })();
+  }, []);
+  return '';
+}
